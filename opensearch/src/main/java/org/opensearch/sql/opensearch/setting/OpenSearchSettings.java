e7db5903f9921ff8fdfc640cd0eafba20d11f4d1
Heng Qian <qianheng@amazon.com>
2026-04-20 14:38:15 +0800

    feat: add plugins.calcite.index_insight_statistics.enabled setting (default false)
    Signed-off-by: Heng Qian <qianheng@amazon.com>

--- opensearch/src/main/java/org/opensearch/sql/opensearch/setting/OpenSearchSettings.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/setting/OpenSearchSettings.java
@@ -186,6 +186,13 @@ public class OpenSearchSettings extends Settings {
           Setting.Property.NodeScope,
           Setting.Property.Dynamic);
 
+  public static final Setting<?> INDEX_INSIGHT_STATISTICS_ENABLED_SETTING =
+      Setting.boolSetting(
+          Key.INDEX_INSIGHT_STATISTICS_ENABLED.getKeyValue(),
+          false,
+          Setting.Property.NodeScope,
+          Setting.Property.Dynamic);
+
   public static final Setting<?> QUERY_MEMORY_LIMIT_SETTING =
       Setting.memorySizeSetting(
           Key.QUERY_MEMORY_LIMIT.getKeyValue(),
@@ -470,6 +477,12 @@ public class OpenSearchSettings extends Settings {
     register(
         settingBuilder,
         clusterSettings,
+        Key.INDEX_INSIGHT_STATISTICS_ENABLED,
+        INDEX_INSIGHT_STATISTICS_ENABLED_SETTING,
+        new Updater(Key.INDEX_INSIGHT_STATISTICS_ENABLED));
+    register(
+        settingBuilder,
+        clusterSettings,
         Key.QUERY_MEMORY_LIMIT,
         QUERY_MEMORY_LIMIT_SETTING,
         new Updater(Key.QUERY_MEMORY_LIMIT));
@@ -658,6 +671,7 @@ public class OpenSearchSettings extends Settings {
         .add(CALCITE_PUSHDOWN_ENABLED_SETTING)
         .add(CALCITE_PUSHDOWN_ROWCOUNT_ESTIMATION_FACTOR_SETTING)
         .add(CALCITE_SUPPORT_ALL_JOIN_TYPES_SETTING)
+        .add(INDEX_INSIGHT_STATISTICS_ENABLED_SETTING)
         .add(DEFAULT_PATTERN_METHOD_SETTING)
         .add(DEFAULT_PATTERN_MODE_SETTING)
         .add(DEFAULT_PATTERN_MAX_SAMPLE_COUNT_SETTING)
