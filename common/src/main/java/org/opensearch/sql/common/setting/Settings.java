03d7a0080a6b5f7a44c8b31c999d5e5d564329c8
Heng Qian <qianheng@amazon.com>
2026-04-24 11:28:02 +0800

    style: wrap long enum key entry
    Line wrap applied by spotless on the setting key added in d5015b593c.
    
    Signed-off-by: Heng Qian <qianheng@amazon.com>

--- common/src/main/java/org/opensearch/sql/common/setting/Settings.java
+++ common/src/main/java/org/opensearch/sql/common/setting/Settings.java
@@ -47,7 +47,8 @@ public abstract class Settings {
     TABLE_STATISTICS_ENABLED("plugins.calcite.table_statistics.enabled"),
     TABLE_STATISTICS_REFRESH_INTERVAL("plugins.calcite.table_statistics.refresh_interval"),
     TABLE_STATISTICS_TTL("plugins.calcite.table_statistics.ttl"),
-    TABLE_STATISTICS_REFRESH_MAX_IN_FLIGHT("plugins.calcite.table_statistics.refresh_max_in_flight"),
+    TABLE_STATISTICS_REFRESH_MAX_IN_FLIGHT(
+        "plugins.calcite.table_statistics.refresh_max_in_flight"),
 
     /** Query Settings. */
     FIELD_TYPE_TOLERANCE("plugins.query.field_type_tolerance"),
